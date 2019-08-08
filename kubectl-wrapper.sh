82d08122a19df465e5a9c05f88d9e252e54dc5ae
Tony Klinakis <a.klinakis@yahoo.com>
2019-08-08 19:12:21 +0100

    #1 Fix syntax issues.

--- kubectl-wrapper.sh
+++ kubectl-wrapper.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-ARGS=["$@"]
-
 # `man highlight` for options.
 HL_OUTPUT="-O truecolor"
 # Sets the input format of highlight.
 HL_SYNTAX_PARAM=
-# Pipe final output through this command
-#POST=| less
 
 
-for arg in $(ARGS)
+# TODO: Support space between arguments - `-o yaml`
+
+for arg in "$@"
+do
 case $arg in
   "-oyaml")
     ;&
   "-o=yaml")
-    $HL_SYNTAX_PARAM="-S yaml"
+    HL_SYNTAX_PARAM="-S yaml"
     break
     ;;
   "-ojson")
     ;&
   "-o=json")
-    $HL_SYNTAX_PARAM="-S json"
+    HL_SYNTAX_PARAM="-S json"
     break
     ;;
   *)
@@ -30,9 +29,9 @@ esac
 
 done
 
-if [-z "$HL_SYNTAX_PARAM"]
+
+if [ -z "$HL_SYNTAX_PARAM" ]
 then
   kubectl "$@"
 else
-  kubectl "$@" | highlight $HL_SYNTAX_PARAM $HL_OUTPUT $HL_TAIL
-fi
+  kubectl "$@" | highlight $HL_SYNTAX_PARAM $HL_OUTPUT
